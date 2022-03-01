d8180bb9ff8a0f806beba48d4101d91d339ea527
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-01 15:40:38 -0600

    add another project functionality

--- app.js
+++ app.js
@@ -36,12 +36,16 @@ return inquirer.prompt([
 };
 
 
-const promptProject = () => {
+const promptProject = portfolioData => {
     console.log(`
     =================
     Add a New Project
     =================
     `);
+   // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+    }
     return inquirer.prompt([
         {
             type: 'input',
@@ -76,10 +80,19 @@ const promptProject = () => {
             message: 'Would you like to enter another project?',
             default: false
         }
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
 };
 
 promptUser()
-    .then(answers => console.log(answers))
-    .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+  });
