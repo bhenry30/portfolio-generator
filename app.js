4c874893d375043e5ad545cb7b156c7161c0bd90
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-04 19:01:16 -0600

    quick edit to copyFile in generate-site.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer')
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
